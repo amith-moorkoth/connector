2b56024e2b1031f4a67a3401277ea0294cdd6ff6
Amith Moorkoth <m.amitkan@gmail.com>
2019-06-22 20:04:43 +0530

    h
    recent

---
+++ precache-manifest.06e342b2e8273cd0164ff727bcc3c664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f211c31ee3b177363341f3e2325a5bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84aff2a91b1d57394dd6",
+    "url": "/static/css/main.5516b039.chunk.css"
+  },
+  {
+    "revision": "79bb68c59254432b05c7",
+    "url": "/static/js/2.50507ac2.chunk.js"
+  },
+  {
+    "revision": "84aff2a91b1d57394dd6",
+    "url": "/static/js/main.663f695b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
